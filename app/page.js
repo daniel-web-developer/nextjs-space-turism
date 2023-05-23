da8e8c9ac8d77786f3016bf37c7ae34b43309bb1
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-05-23 16:35:15 +0200

    organised scss better

--- app/page.js
+++ app/page.js
@@ -23,9 +23,9 @@ export default function Home() {
           <h1>SPACE</h1>
           <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
         </div>
-        <span className="flex flex-justcont-c flex-alignit-c home-circle">
+        <button className="flex flex-justcont-c flex-alignit-c home-circle">
           <h4>EXPLORE</h4>
-        </span>
+        </button>
       </div>
     </Layout>
   )
